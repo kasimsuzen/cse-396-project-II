2b3137755284a98afa240f1f592c0c4a5d998a60
Kasım <kasmszen@gmail.com>
2016-05-13 00:32:27 +0300

    Traffic sign recognition and car detection added

--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@
 #include <string>
 #include <cstdlib>
 #include <cmath>
-
+#include "hardware.h"
 #include "opencv2/opencv.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/features2d/features2d.hpp"
 #include "opencv2/video/tracking.hpp"
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/ml/ml.hpp>
+#include "opencv2/objdetect.hpp"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -17,7 +21,7 @@
 using namespace std;
 using namespace cv;
 
-#define MINUTE 7			// video baslangic dakikasi (1, 4, 9, 14, 15, 16, 16.9, 17, 18, 19 serit degistirme for Yol01.avi) (10, 17.5 Serit degistirme ;; 21 emniyet seridi AnkaraYolu)
+#define MINUTE 5			// video baslangic dakikasi (1, 4, 9, 14, 15, 16, 16.9, 17, 18, 19 serit degistirme for Yol01.avi) (10, 17.5 Serit degistirme ;; 21 emniyet seridi AnkaraYolu)
 #define MIN_LINE_LENGTH 20  // canny icin min line uzunlugu
 #define MAX_LINE_GAP 5      // canny icin max gap size
 #define THRESHOLD_VALUE 100 // FOR LINE
@@ -28,6 +32,14 @@ using namespace cv;
 #define LANE_CHANGE_W 150	// sagdan ve soldan kesilen alan
 #define RESIZE_WIDTH 360    // resize genisligi
 #define RESIZE_HEIGHT 240   // resize yuksekligi
+#define T_RESIZE_WIDTH 640    // tabela icin resize genisligi
+#define T_RESIZE_HEIGHT 480   // tabela icin resize yuksekligi
+#define GREEN CV_RGB(0,255,0)
+
+string cars_cascade_name = "cars3.xml";
+CascadeClassifier cars_cascade;
+string screen;
+
 typedef struct
 {
 	vector<float> Operatos;
@@ -38,28 +50,48 @@ double findLineLength(const Point pt1, const Point pt2);
 bool intersection(Point2f o1, Point2f p1, Point2f o2, Point2f p2, Point2f& r);
 bool intersection(Vec4i line1, Vec4i line2, Point2f& iPoint);
 static double drawOptFlowMap(const Mat& flow, Mat& cflowmap, int step, double, const Scalar& color);
+void findSigns(Mat& image, vector<Rect> &rects);
+string detectCars(Mat& frame1, vector<Point>& elipses, vector<Rect>& cars);
+string intToString(int number);
 
 int main()
 {
+	//hardwareSetup();   // main de 1 defa cagirilir
+	//reset();
 	VideoCapture capture;
 	Mat frame, tempframe, cdst;
 	Mat prevgray, gray, flow, cflow;
 	capture.open("C:/Users/Murat/Desktop/Proje2/AnkaraYolu.avi");
 	capture.set(CV_CAP_PROP_POS_MSEC, MINUTE * 60 * 1000);
+	//-- 1. Load the cascades
+	if (!cars_cascade.load(cars_cascade_name)){
+		printf("--(!)Error loading face cascade\n");
+		return -1;
+	};
+	//-- 2. Read the video stream
+
 	namedWindow("original", 0);
 	//namedWindow("producted", 0);
 	int allFrameTotalMagnitude = 0;
 	int magnitudeArray[30];
 	string speedText = "";
 
+	vector<Rect> rects;
+	vector<Point> elips;
+	vector<Rect> cars;
+	string detectCarsResult;
 	Mat roi;
 	Mat roi2;
 	for (int i = 0; true; i++)
 	{
 		capture >> frame;
+		findSigns(frame, rects);
 		//if(i % 3 != 0) continue;
 		//resize(frame, frame, Size(frame.cols / 2, frame.rows / 2));
 		resize(frame, frame, Size(RESIZE_WIDTH, RESIZE_HEIGHT));
+		elips.clear();
+		cars.clear();
+		detectCarsResult = detectCars(frame, elips, cars);
 		roi = frame.rowRange(CROP_H, RESIZE_HEIGHT).colRange(CROP_W, frame.cols - CROP_W).clone();
 		roi2 = frame.rowRange(SPEED_CROP_H, RESIZE_HEIGHT).colRange(CROP_W, frame.cols - CROP_W).clone();
 		//tempframe = roi;// (Rect(0, 0, frame.cols, frame.rows * 3 / 6));
@@ -68,10 +100,10 @@ int main()
 		cvtColor(roi, gray, CV_BGR2GRAY);            // hiz icin
 		//GaussianBlur(tempframe, tempframe, Size(3, 3), 5, 5);
 		threshold(gray, tempframe, THRESHOLD_VALUE, 255, CV_THRESH_BINARY);
-//		imshow("threshold", tempframe); // threshold ekrani
+		//		imshow("threshold", tempframe); // threshold ekrani
 		Canny(tempframe, tempframe, 50, 255, 3);
-//		imshow("Canny", tempframe);
-		
+		//		imshow("Canny", tempframe);
+
 		//cout << "after canny" << endl;
 
 #if 1
@@ -86,9 +118,9 @@ int main()
 		//line(frame, sou, sau, Scalar(0, 255, 255), 2, CV_AA); // ust
 		//line(frame, soa, saa, Scalar(0, 255, 255), 2, CV_AA); // alt
 
-		for (size_t i = 0; i < lines.size(); i++)
+		for (size_t h = 0; h < lines.size(); h++)
 		{
-			Vec4i l = lines[i];
+			Vec4i l = lines[h];
 			Point pt1, pt2;
 			//int length = findLineLength(pt1, pt2);
 
@@ -101,7 +133,7 @@ int main()
 			{
 				//line( cdst,  pt1, pt2, Scalar(0,0,255), 8, CV_AA );
 				line(frame, pt1, pt2, Scalar(0, 0, 255), 2, CV_AA);
-				fitLines.push_back(lines[i]);
+				fitLines.push_back(lines[h]);
 			}
 
 			if ((angle < 150 && angle > 70) || (angle < -70 && angle > -150))
@@ -117,26 +149,26 @@ int main()
 					cout << "Length of Lane : " << length << endl;
 					line(frame, pt1, pt2, Scalar(225, 10, 15), 2, CV_AA);
 				}
-				fitLines.push_back(lines[i]);
+				fitLines.push_back(lines[h]);
 			}
 
 			if (angle > 21 && angle < 60) // sagdaki seritler (yesil)
 			{
 				//line( cdst,  pt1, pt2, Scalar(0,255,0), 3, CV_AA );
 				line(frame, pt1, pt2, Scalar(0, 255, 0), 2, CV_AA);
-				fitLines.push_back(lines[i]);
+				fitLines.push_back(lines[h]);
 			}
 		}
 
 
-		for (size_t i = 0; i < fitLines.size(); i++)
+		for (size_t k = 0; k < fitLines.size(); k++)
 		{
 			for (size_t j = 0; j < fitLines.size(); j++)
 			{
 				Point2f p;
-				if (intersection(fitLines[i], fitLines[j], p))
+				if (intersection(fitLines[k], fitLines[j], p))
 				{
-					intersectLines.push_back(fitLines[i]);
+					intersectLines.push_back(fitLines[k]);
 					intersectLines.push_back(fitLines[j]);
 
 					//cout << "intersection point is : " << p << endl;					
@@ -154,12 +186,12 @@ int main()
 			calcOpticalFlowFarneback(prevgray, gray, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
 			cvtColor(prevgray, cflow, CV_GRAY2BGR);
 
-//			cout << "magnitude" << drawOptFlowMap(flow, cflow, 16, 1.5, CV_RGB(0, 255, 0)) << endl;
-//			cout << "index " << i << endl;
+			//			cout << "magnitude" << drawOptFlowMap(flow, cflow, 16, 1.5, CV_RGB(0, 255, 0)) << endl;
+			//			cout << "index " << i << endl;
 
 			allFrameTotalMagnitude += drawOptFlowMap(flow, cflow, 16, 1.5, CV_RGB(0, 255, 0));
-//			cout << "allframeMagnitude" << allFrameTotalMagnitude << endl;
-//			cout << allFrameTotalMagnitude / i << endl;
+			//			cout << "allframeMagnitude" << allFrameTotalMagnitude << endl;
+			//			cout << allFrameTotalMagnitude / i << endl;
 
 			if (i == 30){
 
@@ -181,7 +213,7 @@ int main()
 			}
 			putText(frame, "speed: " + speedText, cv::Point(10, frame.rows - 20), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 255, 255), 1);
 
-//			imshow("frame", frame);
+			//			imshow("frame", frame);
 			//imshow("flow", cflow);
 
 			//waitKey(0); 
@@ -191,8 +223,20 @@ int main()
 			break;
 		std::swap(prevgray, gray);
 #endif
-		
+		// tabela icin
+		for (size_t t = 0; t < rects.size(); t++)
+		{
+			rectangle(frame, rects[t], Scalar(123, 159, 20), 3);
+		}
+
+		// araba yakalamak icin
+		putText(frame, detectCarsResult, cv::Point(0, 15), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 255, 0), 1);
+			for (size_t y = 0; y < cars.size(); y++)
+			{
+				ellipse(frame, elips[y], Size(cars[y].width / 2, cars[y].height / 2), 0, 0, 360, Scalar(255, 0, 255), 2, 8, 0);
+			}
 		imshow("original", frame);
+		rects.clear();
 //		imshow("roi", roi);
 //		imshow("roi2", roi2);
 		if (waitKey(30) >= 0) break;
@@ -257,4 +301,111 @@ static double drawOptFlowMap(const Mat& flow, Mat& cflowmap, int step, double, c
 		circle(cflowmap, Point(x, y), 2, color, -1);
 		}
 	return totalMagnitude;
+}
+
+void findSigns(Mat& image, vector<Rect> &rects)
+{
+	Mat src = image.clone();
+
+	resize(src, src, Size(640, 480)); // resize
+	if (src.empty())
+		return;
+
+	Mat orig_image = src.clone();
+	vector<Vec4i> hierarchy;
+	vector<vector<Point> > contours;
+	Mat canny_output;
+	medianBlur(src, src, 3);
+
+	// Convert input image to HSV
+	Mat hsv_image;
+	cvtColor(src, hsv_image, COLOR_BGR2HSV);
+
+	// Threshold the HSV image, keep only the red pixels
+	Mat lower_red_hue_range;
+	Mat upper_red_hue_range;
+	Mat red_hue_image, blue_hue_image;
+
+	//red
+	inRange(hsv_image, Scalar(0, 90, 90), Scalar(15, 255, 255), lower_red_hue_range);
+	inRange(hsv_image, Scalar(150, 100, 100), Scalar(185, 255, 255), upper_red_hue_range);
+	addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
+
+	//blue
+	inRange(hsv_image, Scalar(107, 90, 70), Scalar(140, 255, 255), blue_hue_image);
+	addWeighted(blue_hue_image, 1.0, red_hue_image, 1.0, 0.0, blue_hue_image);
+	GaussianBlur(blue_hue_image, blue_hue_image, Size(9, 9), 2, 2);
+
+	Canny(blue_hue_image, canny_output, 100, 100 * 2, 3);
+	findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+	if (contours.size() == 0)
+		return;
+
+	Mat smallImage = orig_image.clone();
+	for (size_t current_circle = 0; current_circle < contours.size(); ++current_circle)
+	{
+
+
+		if (contourArea(contours[current_circle]) > 800 || contourArea(contours[current_circle]) < 150) continue;
+
+		rectangle(orig_image, boundingRect(Mat(contours[current_circle])), Scalar(255, 255, 0), 2);
+		Rect r = boundingRect(Mat(contours[current_circle]));
+		Point p1((r.tl().x*(double)(0.56)), r.tl().y / 2);
+		Point p2((r.br().x*(double)(0.56)), r.br().y / 2);
+
+		resize(smallImage, smallImage, Size(360, 240));
+		Rect rson(p1, p2);
+		rects.push_back(rson);
+	}
+}
+
+string detectCars(Mat& frame1, vector<Point>& elipses, vector<Rect>& cars)
+{
+	Mat roi, frame_gray, frame;
+	frame = frame1.clone();
+	int carSize = 0;
+	roi = frame.rowRange(75, frame.rows).colRange(20, frame.cols).clone();
+//	imshow("ercan", roi);
+	cvtColor(roi, frame_gray, COLOR_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+
+	//-- Detect cars
+	cars_cascade.detectMultiScale(frame_gray, cars, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(20, 21));
+	for (size_t i = 0; i < cars.size(); i++)
+	{
+		cars[i].y += 75;
+		cars[i].x += 20;
+		Point center(cars[i].x + cars[i].width / 2, cars[i].y + cars[i].height / 2);
+//		ellipse(frame, center, Size(cars[i].width / 2, cars[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+		elipses.push_back(center);
+	}
+
+	/*CAR DETECTING AND SHOW ON SCREEN*/
+	carSize = cars.size();
+	screen.clear();
+	screen = "CARS: " + intToString(carSize);
+
+	if (carSize <= 0){
+		screen +=  " Trafik yok.";
+	}
+	else if (carSize <= 2){
+		screen += " Trafik normal.";
+	}
+	else{
+		screen +=  " Trafik VAR!";
+	}
+	/*Put text on screen*/
+	//putText(frame, screen, cv::Point(0, 15), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 255, 0), 1);
+	//waitKey(30);
+	printf("\tTotal vehicles on screen: %d\n", carSize);
+	//memset(screen, '\0', strlen(screen));
+	return screen;
+}
+
+// numarayi stringe ceviren fonksiyon 
+string intToString(int number){
+	std::stringstream ss;
+	ss << number;
+	return ss.str();
 }
